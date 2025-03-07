86826692ad4c447d41196b41c6d0d1250b0c9a80
Rishabh Singh <148595858+rishi-singh15@users.noreply.github.com>
2025-03-07 17:37:04 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI Assitant by Rishabh (Rishi) Singh`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
 export const PAGE_TITLE: string = `Practicality`;
-export const PAGE_DESCRIPTION: string = `Chat with Practicality, Rishi's AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME, OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
