1285804d94ffb916527b311afbad370cf4e8bc95
Rishabh Singh <148595858+rishi-singh15@users.noreply.github.com>
2025-03-07 17:31:03 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Welcome. Meet Practicality.`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
+export const FOOTER_MESSAGE: string = `Customized AI Assitant by Rishabh (Rishi) Singh`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_TITLE: string = `Practicality`;
+export const PAGE_DESCRIPTION: string = `Chat with Practicality, Rishi's AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
